75307683833a54ba6f4affbce893e86f62a4212b
Pierpaolo Frasa <pfrasa@smail.uni-koeln.de>
2012-04-09 06:38:45 +0200

    Initial commit. Browser CMS initialized.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stuts::Application.config.secret_token = 'c6c8710eb374c1355fe5352a00e7727219f35629ddee50d8f116d1c8607b651f48c4e37ac5feeab6d91dbf08787d8d217d3bc27b9d0058e47ca3d9cd9679f340'
---
+++ db/migrate/20120409043341_browsercms340.rb
@@ -0,0 +1,61 @@
+# Upgrade to Browsercms v3.4.0
+require 'cms/upgrades/v3_4_0'
+class Browsercms340 < ActiveRecord::Migration
+  include Cms::Upgrades::V3_4_0::SchemaStatements
+
+  def change
+    # Prefix the correct namespace where class_names are not prefixed
+    %w[HtmlBlock Category CategoryType Portlet FileBlock ImageBlock Tag].each do |content_type|
+      update_content_types(content_type)
+      update_connectors_table(content_type)
+    end
+
+    update_sitemap
+    update_files
+    standardize_foreign_keys_from_versions_tables_to_original_table
+  end
+
+
+  private
+
+  def namespace_model(name)
+    "Cms::#{name}"
+  end
+
+  def update_files
+    %w[FileBlock ImageBlock].each do |content_type|
+      Cms::AbstractFileBlock.update_all("type = '#{prefix(content_type)}'", "type = '#{content_type}'")
+    end
+  end
+
+  def update_sitemap
+    %w[Section Page Link Attachment].each do |addressable|
+      Cms::SectionNode.where(:node_type=>addressable).each do |node|
+        node.node_type = namespace_model(addressable)
+        node.save!
+      end
+    end
+  end
+
+  def update_content_types(name)
+    found = Cms::ContentType.named(name).first
+    if found
+      found.name = namespace_model(name)
+      found.save!
+    end
+  end
+
+  def update_connectors_table(name)
+    Cms::Connector.where(:connectable_type => name).each do |connector|
+      connector.connectable_type = prefix(name)
+      connector.save!
+    end
+  end
+
+  def standardize_foreign_keys_from_versions_tables_to_original_table
+    models = %w[attachment dynamic_view file_block html_block link page ]
+    models.each do |model|
+      standardize_version_id_column(model)
+    end
+  end
+end
