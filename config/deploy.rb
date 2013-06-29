6608c4c5a2f345d3a6e60c9542aa8bb7b9c2731c
Pierpaolo Frasa <pfrasa@smail.uni-koeln.de>
2013-06-29 23:19:18 +0200

    capify for easier deployment

---
+++ config/deploy.rb
@@ -0,0 +1,12 @@
+set :application, "stuts-cms"
+set :repository,  "git@github.com:Fryie/stuts-cms"
+server "jwmail.de", :app, :web, :db, :primary => true
+
+set :scm, :git
+set :deploy_to, "/var/www/web4/html/stuts-cms"
+set :user, "piru"
+set :branch, "master"
+
+default_run_options[:pty] = true
+
+set :deploy_via, :remote_cache
